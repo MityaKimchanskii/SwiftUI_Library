f84d6e0921718335b48e935579cb7747ac7e7df1
Dmitrii Kim <mitya_kim@icloud.com>
2023-12-28 18:49:35 -0800

    Add RGBullsEye

---
+++ RGBullsYey/RGBullsYey/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  RGBullsYey
+//
+//  Created by Mitya Kim on 12/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
+    @State var showAlert = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color")
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess*255)) G: \(Int(gGuess*255)) B: \(Int(bGuess*255))")
+                }
+            }
+            Button(action: { self.showAlert = true}) {
+                Text("Hit me!")
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text(String(computeScore())))
+            }.padding()
+            
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
+            
+        }
+    }
+    
+    func computeScore() -> Int {
+        let rDiff = rGuess - rTarget
+        let gDiff = gGuess - gTarget
+        let bDiff = bGuess - bTarget
+        let diff = sqrt(rDiff*rDiff + gDiff*gDiff + bDiff*bDiff)
+        return Int((1.0-diff)*100.0+0.5)
+    }
+}
+
+#Preview {
+    ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+        .previewLayout(.fixed(width: 500, height: 320))
+}
+
+struct ColorSlider: View {
+    
+    @Binding var value: Double
+    var textColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0").foregroundStyle(.red)
+            Slider(value: $value)
+            Text("255").foregroundStyle(.red)
+        }.padding(.horizontal)
+    }
+}
---
+++ RGBullsYey/RGBullsYey/RGBullsYeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsYeyApp.swift
+//  RGBullsYey
+//
+//  Created by Mitya Kim on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsYeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+        }
+    }
+}
