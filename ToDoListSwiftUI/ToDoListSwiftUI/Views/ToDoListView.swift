308b4178dc802fff569b27951a39ed9d696561ad
Dmitrii Kim <mitya_kim@icloud.com>
2024-09-21 08:10:59 -0700

    lesson 7

--- ToDoListSwiftUI/ToDoListSwiftUI/Views/ToDoListView.swift
+++ ToDoListSwiftUI/ToDoListSwiftUI/Views/ToDoListView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    
+    @StateObject var viewModel = ToDoListViewViewModel()
+    
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
     var body: some View {
-        Text("Hello World! This is my account!")
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationTitle("ToDo List")
+            .toolbar {
+                Button {
+                    // Action
+                } label: {
+                    Image(systemName: "plus")
+                }
+
+            }
+        }
     }
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "")
 }
