a92b1c2a6a5c8bd790d9e8d3ad8df4814b2beb65
Dmitrii Kim <mitya_kim@icloud.com>
2024-11-25 15:40:46 -0800

    Add new CryptoCurrencyApp

---
+++ CryptoCurrencyApp/CryptoCurrencyApp/CryptoCurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyAppApp.swift
+//  CryptoCurrencyApp
+//
+//  Created by Mitya Kim on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
